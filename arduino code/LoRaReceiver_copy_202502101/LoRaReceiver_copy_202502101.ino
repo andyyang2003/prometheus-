0168e01d6acfe48a0dcbfc7390013684e9efb363
andyyang2003 <ayang11@uci.edu>
2025-02-23 16:27:26 -0800

    got gps working changed to hardwareserial, must calculate average data

--- arduino code/LoRaReceiver_copy_202502101/LoRaReceiver_copy_202502101.ino
+++ arduino code/LoRaReceiver_copy_202502101/LoRaReceiver_copy_202502101.ino
@@ -12,25 +12,22 @@ SSD1306Wire  m_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OL
 /* display end */
 
 /* lora/radio */
+/* radio */
 #define RF_FREQUENCY                                915000000 // Hz
-
-#define TX_OUTPUT_POWER                             14        // dBm
-
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
-                                                              //  1: 250 kHz,
-                                                              //  2: 500 kHz,
-                                                              //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,
-                                                              //  2: 4/6,
-                                                              //  3: 4/7,
-                                                              //  4: 4/8]
+#define TX_OUTPUT_POWER                             20        // dBm 2.23 was 5 turned to 20
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR                       9         // [SF7..SF12] was 7 turned to 9
+#define LORA_CODINGRATE                             4         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
 #define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
 #define LORA_SYMBOL_TIMEOUT                         0         // Symbols
 #define LORA_FIX_LENGTH_PAYLOAD_ON                  false
 #define LORA_IQ_INVERSION_ON                        false
 #define RX_TIMEOUT_VALUE                            1000
 #define BUFFER_SIZE                                 60 // Define the payload size here
+#define SLEEP_TIME_MINUTES 0.5
+#define SLEEP_TIME_SECONDS SLEEP_TIME_MINUTES * 60
+#define SLEEP_TIME_MICROSECONDS SLEEP_TIME_SECONDS * 1000000LL
+#define TXINITNUMBER 15
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
 static RadioEvents_t RadioEvents;
--- arduino code/LoRaSender_copy_20250210183952/LoRaSender_copy_20250210183952.ino
+++ arduino code/LoRaSender_copy_20250210183952/LoRaSender_copy_20250210183952.ino
@@ -6,54 +6,23 @@
 #include "DHT.h"
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
+//#include "esp_rtc_mem.h"
 
 #define DISPLAY
 #define DEBUG
 
-/* PIN DEFINITIONS */
+/* SENSOR PIN DEFINITIONS */
 #define DHTPIN 26
 #define iled 33
 #define dustAnalogOut 7
-
-#define GPS_RX 48 // NOT USED 
-#define GPS_TX 4
-
+#define GPS_RX 4 // NOT USED 
+#define GPS_TX -1 //not used to send data out
 #define mqAnalog 6
+/* SENSOR PIN DEFINITION END */
 
-/* radio */
-#define RF_FREQUENCY                                915000000 // Hz
-#define TX_OUTPUT_POWER                             5        // dBm
-#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
-                                                              //  1: 250 kHz,
-                                                              //  2: 500 kHz,
-                                                              //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,
-                                                              //  2: 4/6,
-                                                              //  3: 4/7,
-                                                              //  4: 4/8]
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
-#define LORA_IQ_INVERSION_ON                        false
-#define RX_TIMEOUT_VALUE                            1000
-#define BUFFER_SIZE                                 60 // Define the payload size here
-static RadioEvents_t RadioEvents;
-void OnTxDone( void );
-void OnTxTimeout( void );
-bool lora_idle=true;
-char txpacket[BUFFER_SIZE];
-char rxpacket[BUFFER_SIZE];
-double txNumber;
-/*radio end */
-
-/* display */
-SSD1306Wire  m_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
-void VextON(void);
-/* display end */
+/* SENSOR CONSTANTS ---------------------------------------------------- */
 
 /* dht */
-
 float currentTemp;
 float currentHumidity;
 char humidityString[6]; // 100.00%
@@ -65,6 +34,7 @@ DHT dht(DHTPIN, DHT22);
 #define COV_RATIO 0.2
 #define NO_DUST_VOLTAGE 400 //baseline threshold
 #define SYS_VOLTAGE 3300 // 3.3V for esp32
+#define GASTHRESHOLD 1500
 float dustdensity, dustvoltage;
 int adcvalue;
 unsigned long previousDustMillis = 0;
@@ -72,17 +42,148 @@ const long dustInterval = 280; // Time between dust readings (ms)
 float dustDensity;
 /*dust sensor end */
 
-/* gps */
-SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
-TinyGPSPlus gps;
-double lat, lon;
-/* gps end */
-
 /*gas sensor*/
 int gasDetected = 0; // 0 if no gas, 1 if gas.
 /*gas sensor end*/
 
-int Filter(int m) //filters data and takes average
+/* SENSOR CONSTANTS END ---------------------------------------------------- */
+
+/* radio */
+#define RF_FREQUENCY                                915000000 // Hz
+#define TX_OUTPUT_POWER                             20        // dBm 2.23 was 5 turned to 20
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR                       9         // [SF7..SF12] was 7 turned to 9
+#define LORA_CODINGRATE                             4         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 60 // Define the payload size here
+#define SLEEP_TIME_MINUTES 0.5
+#define SLEEP_TIME_SECONDS SLEEP_TIME_MINUTES * 60
+#define SLEEP_TIME_MICROSECONDS SLEEP_TIME_SECONDS * 1000000LL
+#define TXINITNUMBER 15
+static RadioEvents_t RadioEvents;
+void OnTxDone( void );
+void OnTxTimeout( void );
+bool lora_idle=true;
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+RTC_DATA_ATTR unsigned int txNumber = 0;
+/*radio end */
+
+/* display */
+SSD1306Wire  m_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+void VextON(void);
+/* display end */
+
+/* gps */
+HardwareSerial gpsSerial(2);
+TinyGPSPlus gps;
+double lat, lon;
+/* gps end */
+
+
+
+// Function Prototypes
+
+void setup();
+void loop();
+void readDHT();
+void readDustSensor();
+void readGPS();
+void gasSensor();
+int filterData(int m);
+void sendLoRaPacket();
+void displayData(const char *message);
+void VextOn();
+void VextOff();
+void vextOff();
+void onTxDone();
+void onTxTimeout();
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Mcu.begin();
+
+#ifdef DISPLAY
+  VextON();
+  m_display.init();
+  m_display.clear();
+  m_display.display();
+#endif
+  
+  //txNumber=0;
+  pinMode(iled, OUTPUT);
+  digitalWrite(iled, LOW); // internal led for dust sensing off initially
+
+  RadioEvents.TxDone = OnTxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+  
+  Radio.Init( &RadioEvents );
+  Radio.SetChannel( RF_FREQUENCY );
+  Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                  LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                  LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                  true, 0, 0, LORA_IQ_INVERSION_ON, 3000 ); 
+
+  dht.begin();
+  gpsSerial.begin(9600, SERIAL_8N1, 4, -1);
+  pinMode(mqAnalog, INPUT);
+}
+
+void loop()
+{
+  readDHT();
+  readDustSensor();
+  readGPS();
+  gasSensor();
+// packet - temperature, humidity, dust density, gps location
+	if(lora_idle == true)
+	{
+    delay(2000); // sends every 2 seconds
+		txNumber += 1;
+    if (txNumber > 99)
+    {
+      txNumber = 0;
+    }
+    //payload: tx#, temp, hum, dust content, lat, long, gas detect
+    /* INTEGER TYPES */
+    int32_t latInt = (int32_t)(lat * 100); // Increased precision
+    int32_t lonInt = (int32_t)(lon * 100);
+    uint16_t tempInt = (uint16_t)(currentTemp * 100);
+    uint16_t humInt = (uint16_t)(currentHumidity * 100);
+    uint16_t dustInt = (uint16_t)(dustdensity * 100);
+    uint16_t txInt = (uint16_t)txNumber;
+    uint8_t gasInt = (uint8_t)gasDetected;
+    sprintf(txpacket,"%u %u %u %u %d %d %u" ,txNumber, tempInt, humInt, dustInt, latInt, lonInt, gasInt);  //start a package
+
+    #ifdef DISPLAY
+		Serial.printf("\r\nsending packet \"%s\" , length %d\r\n", txpacket, strlen(txpacket));
+    m_display.clear();
+    m_display.display();
+    m_display.drawString(0, 0, txpacket);
+    m_display.display();
+    #endif
+		Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out	
+    lora_idle = false;
+	}
+  Radio.IrqProcess( ); // interrupt driven
+  /*
+  if (txNumber >= TXINITNUMBER)
+  {
+    esp_sleep_enable_timer_wakeup(SLEEP_TIME_MICROSECONDS);
+    Serial.println("Going to sleep now");
+    Serial.flush(); // Ensure serial data is sent before sleep
+    esp_deep_sleep_start();
+  }*/
+  
+}
+
+int filterData(int m) //filters data and takes average
 {
   static int flag = 0;
   static int _buff[10], sum;
@@ -112,8 +213,8 @@ int Filter(int m) //filters data and takes average
   }
 }
 void gasSensor() {
-  int gassensorAnalog = analogRead(mqAnalog);
-  if (gassensorAnalog > 1000)
+  int gassensorAnalog = analogRead(mqAnalog);sss
+  if (gassensorAnalog > GASTHRESHOLD)
   {
     gasDetected = 1;
   }
@@ -125,14 +226,15 @@ void gasSensor() {
 void readDHT()
 {
   currentTemp = dht.readTemperature();
-  currentHumidity = dht.readHumidity();
+  currentHumidity = dht.readHumidity(); 
   //dtostrf(currentHumidity, 6, 2, humidityString);
   //dtostrf(currentTemperature, 6, 2, temperatureString);
-  
   if (isnan(currentTemp) || isnan(currentHumidity))
   {
     #ifdef DEBUG
     Serial.println(F("Failed to read from DHT"));
+    #endif
+    #ifdef DISPLAY
     m_display.clear();
     m_display.display();
     m_display.drawString(m_display.getWidth()/2-4, m_display.getHeight()/2-16/2, "FAILED");
@@ -140,6 +242,7 @@ void readDHT()
     #endif
     return;
   }
+
 }
 void readDustSensor() { //reads dust sensor
   unsigned long currentMillis = millis(); // get time at function call
@@ -158,9 +261,9 @@ void readDustSensor() { //reads dust sensor
     digitalWrite(iled, LOW);
     delayMicroseconds(43); 
 
-    adcvalue = Filter(adcvalue); // filter just averages values for smooth collection
-    // SYS VOLTAGE DEPENDS ON OUTPUT VOLTAGE - 3.3 for ESP32
-    // ESP32 ADCVALUE is 12 bit!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! make sure divide sys voltage by 4096
+    adcvalue = filterData(adcvalue); // filter just averages values for smooth collection
+    // SYS VOLTAGE DEPENDS ON OUTPUT VOLTAGE - 3.3-5 for ESP32
+    // ESP32 ADCVALUE is 12 bit make sure divide sys voltage by 4096
     dustvoltage = (SYS_VOLTAGE / 4096.0) * adcvalue * 11;
     if (dustvoltage >= NO_DUST_VOLTAGE) {
       dustvoltage -= NO_DUST_VOLTAGE;
@@ -169,8 +272,8 @@ void readDustSensor() { //reads dust sensor
       dustdensity = 0;
     }
 
-    Serial.print("Dust Density: ");
-    Serial.println(dustdensity);
+    //Serial.print("Dust Density: ");
+    //Serial.println(dustdensity);
   }
 }
 
@@ -179,78 +282,20 @@ void VextON(void) // powers on the oled for display
   pinMode(Vext,OUTPUT);
   digitalWrite(Vext, LOW);
 }
-
-void setup() {
-  Serial.begin(115200);
-  Mcu.begin();
-  // display features disabled for power save
-  #ifdef DISPLAY
-  VextON();
-  m_display.init();
-  m_display.clear();
-  m_display.display();
-  #endif
-  
-  txNumber=0;
-  pinMode(iled, OUTPUT);
-  digitalWrite(iled, LOW); // internal led for dust sensing off initially
-
-  RadioEvents.TxDone = OnTxDone;
-  RadioEvents.TxTimeout = OnTxTimeout;
-  
-  Radio.Init( &RadioEvents );
-  Radio.SetChannel( RF_FREQUENCY );
-  Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
-                                  LORA_SPREADING_FACTOR, LORA_CODINGRATE,
-                                  LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
-                                  true, 0, 0, LORA_IQ_INVERSION_ON, 3000 ); 
-
-  dht.begin();
-  gpsSerial.begin(115200);
-
-  pinMode(mqAnalog, INPUT);
-}
-
-void loop()
-{
-  readDHT();
-  readDustSensor();
-  readGPS();
-// packet - temperature, humidity, dust density, gps location
-	if(lora_idle == true)
-	{
-    delay(2000);
-		txNumber += 0.01;
-		//sprintf(txpacket, "txnum: %0.2f, temp: %0.2f, hum: %0.2f", txNumber, currentTemp, currentHumidity);  //start a package
-    sprintf(txpacket,"tx:%0.2f t:%0.2f h:%0.2f d:%0.2f lt:%0.2f ln:%0.2f gas:%d" ,txNumber, currentTemp, currentHumidity, dustdensity, lat, lon, gasDetected);  //start a package
-
-    #ifdef DISPLAY
-		Serial.printf("\r\nsending packet \"%s\" , length %d\r\n", txpacket, strlen(txpacket));
-    m_display.clear();
-    m_display.display();
-    m_display.drawString(0, 0, txpacket);
-    m_display.display();
-    #endif
-		Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out	
-    lora_idle = false;
-	}
-  Radio.IrqProcess( ); // interrupt driven
-}
 void readGPS()
 {
   static unsigned long prevGPS = 0;
   const unsigned long gpsTimeOut = 50; //50ms
   const int maxBytesToRead = 32;
   int bytesRead = 0;
-  while (gpsSerial.available() > 0 && bytesRead < maxBytesToRead && (millis() - prevGPS) < gpsTimeOut)
+  unsigned long currentTime = millis();
+  if (gpsSerial.available() > 0)
   {
-    if (gps.encode(gpsSerial.read()))
+    if (gps.encode(gpsSerial.read()) > 0)
     {
-      if (gps.location.isValid())
-      {
-        lat = gps.location.lat();
-        lon = gps.location.lng();
-      }
+      lat = gps.location.lat();
+      lon = gps.location.lng();
+      //Serial.printf("%0.2f %0.2f\n", lat, lon);
     }
     bytesRead++;
     prevGPS = millis();
